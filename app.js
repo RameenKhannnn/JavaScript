8dc910992f1603387539228802be21d726903504
RameenKhannnn <rameenkhan8787@gmail.com>
2024-05-20 06:59:24 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+// chap 1//
+alert("Thanks for your input!");
+ 
+// chap 2//
+
+var lastName = "Smith";
+var cityOfOrigin = "New Orleans";
+var aussieGreeting = "g'Day";
+var thanx = "Thanks for your input!"
+ alert(thanx);
+
+ // chap 3 //
+ var originalNum = 23;
+ var newNum = originalNum + 7;
+
+ // chap 4 //
+ var total = 10;
+ total += 5
+total -= 3
+total *= 2
+console.log(total) + 24
+
+
